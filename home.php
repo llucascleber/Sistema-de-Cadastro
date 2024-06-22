ca7298af25e7d0f5235d26ed68b614ac2533e00e
Lucas Cleber <lucas.pollo22@gmail.com>
2024-06-22 13:31:25 -0300

    Sistema_Cadastro

---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compalible" content="IE=edge">
+    <title>SITE</title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+            text-align: center;
+            color: white;
+
+        }
+
+        .box {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 30px;
+            border-radius: 15px;
+        }
+
+        a {
+            text-decoration: none;
+            color: white;
+            border: 3px solid dodgerblue;
+            border-radius: 10px;
+            padding: 10px;
+
+
+        }
+
+        a:hover {
+            background-color: dodgerblue;
+        }
+    </style>
+</head>
+
+<body>
+    <a href="home.php">Voltar</a>
+    <h1>Fomulario de Clientes</h1>
+    <h2>PR1-Projeto interdiciplinar</h2>
+    <div class="box">
+        <a href="">Login</ah>
+            <a href="Formulario.php">Cadastra-se</a>
+
+    </div>
+
+</body>
+
+</html>
